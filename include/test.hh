76d12c484bb892f5c29542c04c7db6aa8cf0f808
Vincent de Marignac <vincentdm_05@yahoo.fr>
2017-08-16 11:45:19 +0300

    add mwe

---
+++ include/test.hh
@@ -0,0 +1,10 @@
+#ifndef TEST_HH
+#define TEST_HH
+
+#include <iostream>
+
+void testFunc() {
+  std::cout << "this is a test" << std::endl;
+}
+
+#endif // TEST_HH
---
+++ src/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "test.hh"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "Hello World!" << endl;
+  testFunc();
+  return 0;
+}
