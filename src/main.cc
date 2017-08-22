60e4fe9cfdaaad9e83da2744e0a871baa4d1f8bd
Vincent de Marignac <vincentdm_05@yahoo.fr>
2017-08-22 22:30:05 +0300

    open an empty window and add useful dependencies

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,48 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "test.hh"
-
-using namespace std;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char *argv[])
 {
-  cout << "Hello World!" << endl;
-  testFunc();
+  if (!glfwInit()) {
+    fprintf(stderr, "Failed to initialise GLFW.\n");
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // For MacOS
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window;
+  window = glfwCreateWindow(1024, 768, "kuvaaja", NULL, NULL);
+  if (window == NULL){
+    fprintf(stderr, "Failed to open GLFW window.\n");
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glewExperimental = true; // Needed in core profile
+  if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW.\n");
+    return -1;
+  }
+
+  // Activate input
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+  do{
+    // Draw nothing for now
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+  } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+  glfwWindowShouldClose(window) == 0);
+
   return 0;
 }
