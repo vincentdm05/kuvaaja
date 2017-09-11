eb65936126ae7f211e58d1d113879410d4977c1f
Vincent de Marignac <vincentdm_05@yahoo.fr>
2017-09-11 11:15:57 +0300

    refactor for consistency

--- src/main.cpp
+++ src/main.cpp
@@ -55,18 +55,18 @@ int main(int argc, char *argv[]) {
   glBindVertexArray(VertexArrayID);
 
   // Test data
-  static const GLfloat triangle_data[] = {
+  static const GLfloat triangleData[] = {
     -1.0f, -1.0f, 0.0f,
     1.0f, -1.0f, 0.0f,
     0.0f,  1.0f, 0.0f
   };
-  const GLuint nVertices = sizeof(triangle_data) / (3 * sizeof(GLfloat));
+  const GLuint nVertices = sizeof(triangleData) / (3 * sizeof(GLfloat));
 
   // Create VBO
-  GLuint vertexbuffer;
-  glGenBuffers(1, &vertexbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_data), triangle_data, GL_STATIC_DRAW);
+  GLuint vertexBuffer;
+  glGenBuffers(1, &vertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(triangleData), triangleData, GL_STATIC_DRAW);
 
   // Load, compile and bind shaders
   std::string shaderFolder = "shaders/";
@@ -100,7 +100,7 @@ int main(int argc, char *argv[]) {
 
     // Draw data
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glDrawArrays(GL_TRIANGLES, 0, nVertices);
     glDisableVertexAttribArray(0);
