dd3964c0de2d944c39eff93451982c95dd52d24d
Vincent de Marignac <vincentdm_05@yahoo.fr>
2018-03-29 19:46:47 +0300

    add debug and release modes

--- compile.sh
+++ compile.sh
@@ -2,8 +2,29 @@
 
 cd "`dirname "$0"`"
 
+for arg in "$@"
+do
+  case "$arg" in
+    -b=*|--build=*)
+    build_type="${arg#*=}"
+    shift
+    ;;
+    *)
+    shift
+    ;;
+  esac
+done
+
+if [ "`echo $build_type | tr -s  '[:upper:]' '[:lower:]'`" = debug ]; then
+  build_type=Debug
+  build_dir=Debug
+else
+  build_type=Release
+  build_dir=Release
+fi
+
 root_dir=`pwd`
-build_dir="$root_dir"/build
+build_dir="$root_dir"/build/$build_dir
 
 if [[ -e "$build_dir" && ! -d "$build_dir" ]]; then
   printf "The build directory cannot be created." 1>&2
@@ -14,5 +35,5 @@ fi
 
 cd "$build_dir"
 
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=$build_type ../..
 make -j `getconf _NPROCESSORS_ONLN`
